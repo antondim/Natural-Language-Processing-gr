b7c1666dba7e1f62edf69ad43ed34f157631d5a4
antondim <dimanton93@gmail.com>
2019-07-02 19:49:40 +0300

    Update getIndexCompressionFactor.m

--- tools/stemmer_greek/evaluation/getIndexCompressionFactor.m
+++ tools/stemmer_greek/evaluation/getIndexCompressionFactor.m
@@ -1,11 +1,11 @@
 function [ ICF ] = getIndexCompressionFactor(corpus,stems )
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Evaluation Metrics for stemmers' strength
 % Based on Frakes Metrics
 %
 % The index compression factor is defined as ICS = n-s/n.
-%   "n" is the number of words in the corpus (corpus).
+%   "n" is the number of words in the corpus (corpus is the total number of tokens extracted from processed text).
 %   "s" is the number of stems produced by the stemmer.
 %
 % Author: Antonopoulos Dimitrios, 2018-2019
@@ -13,7 +13,7 @@ function [ ICF ] = getIndexCompressionFactor(corpus,stems )
 % Note:
 % This metric indicates the total terms reduction that can be achieved through stemming.
 %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 ICF = (length(corpus) - length(unique(stems)))/length(corpus);
 

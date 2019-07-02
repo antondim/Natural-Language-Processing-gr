7aa2c3b250940f398cd73bd796d297e3f50fd587
antondim <dimanton93@gmail.com>
2019-07-02 19:02:33 +0300

    Update endsWithVowel2.m

--- tools/stemmer_greek/necessary_files/endsWithVowel2.m
+++ tools/stemmer_greek/necessary_files/endsWithVowel2.m
@@ -1,4 +1,3 @@
-function [ out ] = endsWithVowel2(token)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Function that checks if input's last letter is a vowel
 % except 'Υ'.
@@ -16,6 +15,8 @@ function [ out ] = endsWithVowel2(token)
 % Part of stemmer script
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [ out ] = endsWithVowel2(token)
 
 vowels = {'Α','Ε','Η','Ι','Ο','Ω'};
 

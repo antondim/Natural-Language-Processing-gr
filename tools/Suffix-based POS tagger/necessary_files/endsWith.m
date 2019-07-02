3ba14feb1eb0231c5f386da5178810263df6f75d
antondim <dimanton93@gmail.com>
2019-07-02 19:12:17 +0300

    Update endsWith.m

--- tools/Suffix-based POS tagger/necessary_files/endsWith.m
+++ tools/Suffix-based POS tagger/necessary_files/endsWith.m
@@ -1,3 +1,5 @@
+function [output] = endsWith(token,list_of_suffixes_to_check)
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Function that checks the suffix of input token
 % looking into a suffix list (suffixesGR.m)
@@ -6,8 +8,6 @@
 %
 % output: True (1) or False (0)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function [output] = endsWith(token,list_of_suffixes_to_check)
 
 suffix_length = cellfun('length',list_of_suffixes_to_check);
 token_length = length(token);
